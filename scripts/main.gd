39e2fa066f9cbd6a274f9d982e2b46217a101d5d
Ziegemon <thilo@familie-mutz.de>
2026-02-16 19:02:04 +0100

    status bar user anzeige

--- scripts/main.gd
+++ scripts/main.gd
@@ -14,6 +14,7 @@ func _ready() -> void:
 	connectBenutzerauswahlSignals()
 	current_selected_user = $user_selection/MarginContainer/VBoxContainer.get_child(0)
 	current_selected_user.userSelected()
+	setUpStatusBar()
 
 #---------------------------------------------------------------------------------------------------
 
@@ -45,3 +46,26 @@ func activateCurrentuser():
 	
 	current_selected_user = $user_selection/MarginContainer/VBoxContainer.get_child(current_user - 1)
 	current_selected_user.userSelected()
+	
+	if current_selected_user is Benutzerauswahl && current_selected_user.user != null:
+		$status_bar/Label_user_id.text = str(current_selected_user.user.user_id)
+		$status_bar/Label_user_name.text = str(current_selected_user.user.username)
+	elif current_selected_user is Benutzerauswahl_extra:
+		$status_bar/Label_user_id.text = "X"
+		$status_bar/Label_user_name.text = "EXTRA"
+	else:
+		$status_bar/Label_user_id.text = "X"
+		$status_bar/Label_user_name.text = "______________________"
+
+#---------------------------------------------------------------------------------------------------
+
+func setUpStatusBar():
+	if current_selected_user is Benutzerauswahl && current_selected_user.user != null:
+		$status_bar/Label_user_id.text = str(current_selected_user.user.user_id)
+		$status_bar/Label_user_name.text = str(current_selected_user.user.username)
+	elif current_selected_user is Benutzerauswahl_extra:
+		$status_bar/Label_user_id.text = "X"
+		$status_bar/Label_user_name.text = "EXTRA"
+	else:
+		$status_bar/Label_user_id.text = "X"
+		$status_bar/Label_user_name.text = "______________________"
