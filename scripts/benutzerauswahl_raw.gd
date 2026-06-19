485abe34de63caeaae3354b8c2f68900f0ef955b
Ziegemon <thilo@familie-mutz.de>
2026-06-19 19:48:13 +0200

    Rechnung drucken ist jetzt schön

--- scripts/benutzerauswahl_raw.gd
+++ scripts/benutzerauswahl_raw.gd
@@ -317,44 +317,141 @@ func printRechnungFromRechnungslisteElement(rl_element : rechnungsliste_element)
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
 
-func actuallyPrintRechnung(rl_element : rechnungsliste_element):
-	#in practice this data would be send to a printer, but I don`t want to buy one just for this sproject
-	
-	print("--------------------------------")
-	
-	print("Bäckerei Kasprovicz")
-	print("Tel: 08153 / 9527091")
-	print("Zum Kuckuksheim 3")
-	print("Wörthsee 82237")
-	
+#func actuallyPrintRechnung(rl_element : rechnungsliste_element):
+	##in practice this data would be send to a printer, but I don`t want to buy one just for this sproject
+	#
+	#print("--------------------------------")
+	#
+	#print("Bäckerei Kasprovicz")
+	#print("Tel: 08153 / 9527091")
+	#print("Zum Kuckuksheim 3")
+	#print("Wörthsee 82237")
+	#
+	#print("")
+	#print("- - - - - - - - - - - - - - - - - - - - - - -")
+	#print("")
+	#
+	##print(str(rl_element.item_list_array.size()))
+	##for i in rl_element.item_list_array:
+		##if i.weight == 0.000: #not a wiegeprodukt
+			##print(i.item.name + "       " + str(i.quantity) + " x" + "       " + str(i.item.price) + "€")
+		##else: #wiegeprodukt
+			##print(i.item.name + "       " + str(i.weight) + " kg" + "       " + str(i.item.price * i.weight) + "€")
+	#
+	#for i in rl_element.item_list_array:
+		#var name = i.item.name
+		#var quantity = ""
+		#var price = ""
+#
+		#if i.weight == 0.0:
+			#quantity = "%d x" % i.quantity
+			#price = "%.2f€" % i.item.price
+		#else:
+			#quantity = "%.3f kg" % i.weight
+			#price = "%.2f€" % (i.item.price * i.weight)
+#
+		#print("%-25s %-10s %10s" % [name, quantity, price])
+	#
+	#print("")
+	#print("- - - - - - - - - - - - - - - - - - - - - - -")
+	#
+	##Auf steuern wird verzichtet, die werden hinterzogen
+	#
+	#if rl_element.rabatt == 0.0:
+		#print(str(rl_element.revenue) + "€")
+	#else:
+		#print("Rabatt: " + str(rl_element.rabatt))
+		#print("Betrag: " + str(rl_element.revenue * rl_element.rabatt) + "€")
+	#
+	#print("- - - - - - - - - - - - - - - - -")
+	#print("")
+	#
+	#var day = Time.get_date_dict_from_system()
+	#print("%02d.%02d.%02d" % [day.day, day.month, day.year])
+	#print("%02d:%02d:%02d" % [rl_element.time.hour, rl_element.time.minute, rl_element.time.second])
+	#print("Es bediente Sie: " + rl_element.user_name)
+	#
+	#print("--------------------------------")
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+
+func center_text(text: String, width: int) -> String:
+	if text.length() >= width:
+		return text
+
+	var total := width - text.length()
+	@warning_ignore("integer_division")
+	var left := total / 2
+	var right := total - left
+
+	return " ".repeat(left) + text + " ".repeat(right)
+
+#-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -    
+
+func format_line(left: String, right: String, width: int) -> String:
+	var spaces := width - left.length() - right.length()
+	return left + " ".repeat(max(1, spaces)) + right
+
+#-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -    
+
+func actuallyPrintRechnung(rl_element: rechnungsliste_element):
+	var width := 45
+
 	print("")
-	print("- - - - - - - - - - - - - - - - -")
 	print("")
+	print("-----------------------------------------------")
+
+	print(center_text("Bäckerei Kasprovicz", width + 2))
+	print(center_text("Tel: 08153 / 9527091", width + 2))
+	print(center_text("Zum Kuckuksheim 3", width + 2))
+	print(center_text("Wörthsee 82237", width + 2))
+
+	print("")
+	print("-----------------------------------------------")
+	print("")
+
+	# ---------------- ITEMS ----------------
 	
-	print(str(rl_element.item_list_array.size()))
 	for i in rl_element.item_list_array:
-		if i.weight == 0.000: #not a wiegeprodukt
-			print(i.item.name + "       " + str(i.quantity) + " x" + "       " + str(i.item.price) + "€")
-		else: #wiegeprodukt
-			print(i.item.name + "       " + str(i.weight) + " kg" + "       " + str(i.item.price * i.weight) + "€")
-	
+		var item_name = i.item.name
+		var quantity = ""
+		var price = ""
+
+		if i.weight == 0.0:
+			quantity = "%d x" % i.quantity
+			price = "%.2f€" % i.item.price
+		else:
+			quantity = "%.3f kg" % i.weight
+			price = "%.2f€" % (i.item.price * i.weight)
+
+		print("%-25s %-10s %10s" % [item_name, quantity, price])
+
 	print("")
-	print("- - - - - - - - - - - - - - - - -")
-	
-	#Auf steuern wird verzichtet, die werden hinterzogen
-	
+	print("-----------------------------------------------")
+
+	# ---------------- SUMME / RABATT ----------------
+
 	if rl_element.rabatt == 0.0:
-		print(str(rl_element.revenue) + "€")
+		print(format_line("Betrag", "%.2f€" % rl_element.revenue, width + 2))
 	else:
-		print("Rabatt: " + str(rl_element.rabatt))
-		print("Betrag: " + str(rl_element.revenue * rl_element.rabatt) + "€")
-	
-	print("- - - - - - - - - - - - - - - - -")
+		print(format_line("Rabatt", str(int(round((1 - rl_element.rabatt) * 100))) + "%", width + 2))
+		print(format_line("Betrag", "%.2f€" % (rl_element.revenue * rl_element.rabatt), width + 2))
+
+	print("-----------------------------------------------")
 	print("")
-	
+
+	# ---------------- DATUM ----------------
 	var day = Time.get_date_dict_from_system()
-	print("%02d.%02d.%02d" % [day.day, day.month, day.year])
-	print("%02d:%02d:%02d" % [rl_element.time.hour, rl_element.time.minute, rl_element.time.second])
-	print("Es bediente Sie: " + rl_element.user_name)
-	
-	print("--------------------------------")
+	var date_str = "%02d.%02d.%02d" % [day.day, day.month, day.year]
+	var time_str = "%02d:%02d:%02d" % [
+		rl_element.time.hour,
+		rl_element.time.minute,
+		rl_element.time.second
+	]
+
+	print(center_text(date_str, width))
+	print(center_text(time_str, width))
+
+	print(center_text("Es bediente Sie: " + rl_element.user_name, width))
+
+	print("-----------------------------------------------")
