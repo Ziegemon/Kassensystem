5a7f61d8e8b299d01e9102cb045495120e310194
Ziegemon <thilo@familie-mutz.de>
2026-06-23 11:14:58 +0200

    schule23.6.26

---
+++ changes Schule/arbeitszeit_data.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name arbeitszeit_data
+
+var date = Time.get_date_dict_from_system()
+var time : float #in minutes
---
+++ changes Schule/user_data.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name user_data
+
+@export var user_id : int
+@export var username : String
+
+var arbeitszeit_datas : Array[arbeitszeit_data] = []
