dade6d08cd6b1d8b940fb515d7808241ea6b98f8
Ziegemon <thilo@familie-mutz.de>
2026-06-24 10:59:33 +0200

    pause button now toggles zeiterfassungs_status
    pasusen toggle visualizer noch adden

--- scripts/main.gd
+++ scripts/main.gd
@@ -690,8 +690,11 @@ func _on_button_abmelden_pressed() -> void:
 func _on_button_pause_pressed() -> void:
 	if current_selected_user.is_benutzerauswahl_extra():
 		return
-		
-	current_selected_user.zeiterfassungs_status = false
+	
+	if current_selected_user.zeiterfassungs_status == true:
+		current_selected_user.zeiterfassungs_status = false
+	else:
+		current_selected_user.zeiterfassungs_status = false
 
 #---------------------------------------------------------------------------------------------------
 
